50cc20d6f40c932f0e3acf9090056d010cc306f3
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-08-05 14:12:21 -0400

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -10,12 +10,13 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 //YOUR FIREBASE LINKS
-Rn = localStorage.getItem("RoomName");
+Roomn = localStorage.getItem("RoomName");
 un = localStorage.getItem("Username!!");
 
 function getData() {
       firebase.database().ref("/" + room_name).on('value', function (snapshot) {
-            document.getElementById("output").innerHTML = "";
+            document.getElementById("MSG").innerHTML = "";
+            document.getElementById("output").value = ""
             snapshot.forEach(function (childSnapshot) {
                   childKey = childSnapshot.key;
                   childData = childSnapshot.val();
@@ -33,7 +34,7 @@ Messa = "<h4 class = 'message_h4'>" + message + "</h4>";
 button_ = "<button class = 'btn btn-danger' id = " + firebase_message_id + " value=" +like + " onclick = 'updateLike(this.id)'>";
 Swtu = "<span class = 'glyphicon glyphicon-thumbs-up'> Likes: " + like +"</span></button></hr>" ;
 row = nameTag + Messa + button_ + Swtu;
-
+document.getElementById("output").innerHTML += row;
                         //End code
                   }
             });
@@ -46,19 +47,19 @@ button_Id = message_id;
 likes = document.getElementById(button_Id).value;
 uplike = Number(likes) + 1;
 console.log(uplike);
-firebase.database().ref(Rn).child(message_id).update({
+firebase.database().ref(Roomn).child(message_id).update({
    likes:uplike   
 });
 }
 function SendMSG() {
       msg = document.getElementById("MSG").value;
-      firebase.database().ref(Rn).push({
+      firebase.database().ref(Roomn).push({
             name: un,
             message: msg,
             like: 0
       });
       document.getElementById("MSG").value = "";
-      document.getElementById("output").innerHTML += row;
+     
 }
 function L0G(){
       localStorage.removeItem("Username!");
